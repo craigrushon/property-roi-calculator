f65dfaa963d822b248dce9acc508f64b903bc1b7
Craig Rushon <craigrushon@craigs-laptop.home>
2024-09-24 15:41:42 -0400

    Add typescript support and examples

---
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { useState, FunctionComponent } from "react";
+import * as style from "./HelloWorld.module.css";
+
+export interface Props {
+  name: string;
+}
+
+const HelloWorld: FunctionComponent<Props> = (props: Props) => {
+  const [name, setName] = useState(props.name);
+
+  return (
+    <div>
+      <h3>Hello, {name}!</h3>
+      <hr />
+      <form>
+        <label className={style.bright} htmlFor="name">
+          Say hello to:
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default HelloWorld;
